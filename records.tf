afa15f6bb6dacd8023f69d566a7f40ac1fa380ff
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-04-15 18:38:28 +0900

    Update domain

--- records.tf
+++ records.tf
@@ -1,13 +1,13 @@
 resource "dnsimple_record" "naked" {
     domain = "tomohiro.me"
-    name   = "tomohiro.me"
+    name   = "@"
     value  = "103.203.90.2"
     type   = "A"
     ttl    = 600
 }
 
 resource "dnsimple_record" "kloudsecwvrgwwvdrp" {
-    domain = "kloudsecwvrgwwvdrp.tomohiro.me"
+    domain = "tomohiro.me"
     name   = "kloudsecwvrgwwvdrp"
     value  = "103.203.90.2"
     type   = "A"
