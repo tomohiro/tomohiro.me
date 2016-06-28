a2e6291457d77188bc33216c7673ba6eee40794e
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-06-28 18:24:30 +0900

    Add AWS credentials

--- config.tf
+++ config.tf
@@ -1,6 +1,9 @@
 variable "atlas_token" {}
 variable "dnsimple_token" {}
 variable "dnsimple_email" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "aws_region" {}
 
 atlas {
     name = "tomohiro/tomohiro.me"
@@ -13,4 +16,10 @@ provider "atlas" {
 provider "dnsimple" {
     token = "${var.dnsimple_token}"
     email = "${var.dnsimple_email}"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
+  region     = "${var.aws_region}"
 }
