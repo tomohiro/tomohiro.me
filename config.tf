6656185c32fe49156437d8ef3fea84256f3341bf
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-07-08 14:20:31 +0900

    Format by `terraform fmt`

--- config.tf
+++ config.tf
@@ -1,21 +1,26 @@
 variable "atlas_token" {}
+
 variable "dnsimple_token" {}
+
 variable "dnsimple_email" {}
+
 variable "aws_access_key_id" {}
+
 variable "aws_secret_access_key" {}
+
 variable "aws_region" {}
 
 atlas {
-    name = "tomohiro/tomohiro.me"
+  name = "tomohiro/tomohiro.me"
 }
 
 provider "atlas" {
-    token = "${var.atlas_token}"
+  token = "${var.atlas_token}"
 }
 
 provider "dnsimple" {
-    token = "${var.dnsimple_token}"
-    email = "${var.dnsimple_email}"
+  token = "${var.dnsimple_token}"
+  email = "${var.dnsimple_email}"
 }
 
 provider "aws" {
