5e9ac97ce7cb69d89eeb9b630f4cf1e936c4631c
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-07-11 09:26:17 +0900

    Remove DNSimple records

--- records.tf
+++ records.tf
@@ -25,19 +25,3 @@ resource "aws_route53_record" "znc" {
   ttl     = "300"
   records = ["128.199.91.91"]
 }
-
-resource "dnsimple_record" "naked" {
-  domain = "tomohiro.me"
-  name   = "@"
-  value  = "103.203.90.2"
-  type   = "A"
-  ttl    = 600
-}
-
-resource "dnsimple_record" "www" {
-  domain = "tomohiro.me"
-  name   = "www"
-  value  = "103.203.90.2"
-  type   = "A"
-  ttl    = 600
-}
