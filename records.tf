9f9041fe0e84aaf1577f1f3d3e94d5372bfc610b
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-07-11 09:25:15 +0900

    Add ZNC record

--- records.tf
+++ records.tf
@@ -12,10 +12,18 @@ resource "aws_route53_record" "naked" {
 
 resource "aws_route53_record" "www" {
   zone_id = "${aws_route53_zone.main.zone_id}"
-  name    = "www"
+  name    = "www.tomohiro.me"
   type    = "A"
   ttl     = "300"
   records = ["103.203.90.2"]
+}
+
+resource "aws_route53_record" "znc" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "znc.tomohiro.me"
+  type    = "A"
+  ttl     = "300"
+  records = ["128.199.91.91"]
 }
 
 resource "dnsimple_record" "naked" {
