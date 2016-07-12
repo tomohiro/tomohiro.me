9ba77a17678c0d137c6fe2d5cc32f104d73cb8d4
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-07-12 23:11:22 +0900

    Add Keybase verify record

--- records.tf
+++ records.tf
@@ -18,6 +18,14 @@ resource "aws_route53_record" "www" {
   records = ["103.203.90.2"]
 }
 
+resource "aws_route53_record" "keybase" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "_keybase.tomohiro.me"
+  type    = "TXT"
+  ttl     = "300"
+  records = ["keybase-site-verification=Poyjvel7Sxt5__4im0nOGQyhurCNpumUXF5YEt_OkwQ"]
+}
+
 resource "aws_route53_record" "znc" {
   zone_id = "${aws_route53_zone.main.zone_id}"
   name    = "znc.tomohiro.me"
